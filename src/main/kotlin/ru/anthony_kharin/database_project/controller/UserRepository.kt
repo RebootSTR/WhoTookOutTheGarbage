85aff54b7186dbf4a6fd025c289943236c51016b
AnthonyKharin <Anthony.Kharin@yandex.ru>
2022-09-10 14:45:17 +0300

    revert

--- src/main/kotlin/ru/anthony_kharin/database_project/controller/UserRepository.kt
+++ src/main/kotlin/ru/anthony_kharin/database_project/controller/UserRepository.kt
@@ -1,7 +1,7 @@
 package ru.anthony_kharin.database_project.controller
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : JpaRepository<UserEntity, Int>
+interface UserRepository : CrudRepository<UserEntity, Int>
